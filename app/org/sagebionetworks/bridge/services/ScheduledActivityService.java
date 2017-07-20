f8f180b189753911adf24454f2ee7c53be5fd5df
Alx Dark <alxdark@gmail.com>
2017-07-20 09:41:21 -0700

    Further work on consolidation and rationalization of list models.

--- app/org/sagebionetworks/bridge/services/ScheduledActivityService.java
+++ app/org/sagebionetworks/bridge/services/ScheduledActivityService.java
@@ -31,6 +31,7 @@ import org.springframework.stereotype.Component;
 import org.sagebionetworks.bridge.dao.ScheduledActivityDao;
 import org.sagebionetworks.bridge.exceptions.BadRequestException;
 import org.sagebionetworks.bridge.models.CriteriaContext;
+import org.sagebionetworks.bridge.models.ForwardCursorPagedResourceList;
 import org.sagebionetworks.bridge.models.schedules.Activity;
 import org.sagebionetworks.bridge.models.schedules.ActivityType;
 import org.sagebionetworks.bridge.models.schedules.CompoundActivity;
@@ -39,7 +40,6 @@ import org.sagebionetworks.bridge.models.schedules.Schedule;
 import org.sagebionetworks.bridge.models.schedules.ScheduleContext;
 import org.sagebionetworks.bridge.models.schedules.SchedulePlan;
 import org.sagebionetworks.bridge.models.schedules.ScheduledActivity;
-import org.sagebionetworks.bridge.models.schedules.ScheduledActivityList;
 import org.sagebionetworks.bridge.models.schedules.ScheduledActivityStatus;
 import org.sagebionetworks.bridge.models.schedules.SchemaReference;
 import org.sagebionetworks.bridge.models.schedules.SurveyReference;
@@ -150,7 +150,7 @@ public class ScheduledActivityService {
         this.surveyService = surveyService;
     }
 
-    public ScheduledActivityList getActivityHistory(String healthCode,
+    public ForwardCursorPagedResourceList<ScheduledActivity> getActivityHistory(String healthCode,
             String activityGuid, DateTime scheduledOnStart, DateTime scheduledOnEnd, String offsetBy,
             int pageSize) {
         checkArgument(isNotBlank(healthCode));
